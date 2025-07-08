d0c3d8c32b5d65a45a331351b4970341c01de968
prajyotgorlewar <prajyotgorlewar@gmail.com>
2025-07-08 23:50:03 +0530

    feat: set up basic routing structure and page layout
    - Configured `react-router-dom` to define application routes.
    - Created `AppRoutes` component to handle routing logic.
    - Implemented a basic layout in `App.jsx` with a header and main content area using Tailwind CSS for styling.
    - Added a default `Landing` component as the index route.

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,14 +1,19 @@
-import React from 'react'
-import Header from './components/Header'
-import { Outlet } from 'react-router-dom'
+import React from 'react';
+import Header from './components/Header';
+import { Outlet } from 'react-router-dom';
 
 const App = () => {
   return (
-    <>
+    <div className="min-h-screen flex flex-col">
+      {/* Site Header */}
       <Header />
-      <Outlet />
-    </>
-  )
-}
 
-export default App
+      {/* Page Content */}
+      <main className="flex-1">
+        <Outlet />
+      </main>
+    </div>
+  );
+};
+
+export default App;
