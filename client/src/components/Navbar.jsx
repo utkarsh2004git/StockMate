48e5b0402a6edded4011ae8cab47ddf98e8d2ba5
ukarsh2004git <utkarshshahare2004@gmail.com>
2025-07-09 20:59:36 +0530

    feat: integrate Clerk authentication buttons into Navbar component

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,5 +1,5 @@
 import logo from "../assets/logo.png";
-
+import { SignedIn, SignedOut, SignInButton, UserButton } from '@clerk/clerk-react'
 export default function Navbar() {
     return (
         <div className="flex justify-between items-center mb-8 px-4 rounded-2xl shadow-md md:px-6 py-2.5 fixed top-0 left-0 right-0 bg-white z-50">
@@ -13,12 +13,21 @@ export default function Navbar() {
             </div>
 
             <div className="flex space-x-4">
-                <button className="hover:cursor-pointer border border-teal-500 text-black font-medium px-4 py-1.5 rounded-full hover:bg-teal-50 transition shadow-sm">
-                    Sign Up
-                </button>
-                <button className="hover:cursor-pointer bg-gray-900 text-white px-4 py-1.5 rounded-full font-medium shadow hover:bg-gray-800 transition">
-                    Login
-                </button>
+                <SignedOut>
+                    <SignInButton>
+                        <button className="hover:cursor-pointer border border-teal-500 text-black font-medium px-4 py-1.5 rounded-full hover:bg-teal-50 transition shadow-sm">
+                            Sign Up
+                        </button>
+                    </SignInButton>
+                    <SignInButton>
+                        <button className="hover:cursor-pointer bg-gray-900 text-white px-4 py-1.5 rounded-full font-medium shadow hover:bg-gray-800 transition">
+                            Login
+                        </button>
+                    </SignInButton>
+                </SignedOut>
+                <SignedIn>
+                    <UserButton/>
+                </SignedIn>
             </div>
         </div>
 
